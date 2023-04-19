fa727a1a63b0c56ae9aabd299c3e0420fb9443e1
Ming Hao Loh <mloh2@wisc.edu>
2023-04-19 16:23:23 -0500

    add logout button

--- TinderFrontend/scenes/profile-scene.js
+++ TinderFrontend/scenes/profile-scene.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import {
-    SafeAreaView, Text
+    SafeAreaView, StyleSheet, Text, TouchableOpacity
 } from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
 export const ProfileScene = () => {
+    const navigation = useNavigation();
+
+    const logout = () => {
+        navigation.navigate("LoginScene");
+    }
+
     return (
-        <SafeAreaView>
-            <Text>Profile Screen</Text>
+        <SafeAreaView style={styles.entireScene}>
+            <TouchableOpacity
+                style={styles.loginButton}
+                title="Logout"
+                onPress={logout}>
+                <Text style={styles.loginButtonText}>Logout</Text>
+            </TouchableOpacity>
         </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    entireScene: {
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center"
+    },
+    loginButton: {
+        backgroundColor: 'pink',
+        borderRadius: 30,
+        width: 340,
+        height: 55,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        opacity: 1,
+        marginVertical: 20,
+    },
+    loginButtonText: {
+        color: 'black',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+});
